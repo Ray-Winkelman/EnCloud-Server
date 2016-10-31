d09aeff7efa764354d9c67b5f9dd69c1fcd37d01
Ray Winkelman <raywinkelman@gmail.com>
2016-10-30 23:43:36 -0300

    auth controller changes + login route/view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\Middleware\Authenticate;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +19,22 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return view('test');
 });
+
+Route::post('register', 'Auth\RegisterController@register');
+
+Route::get('/check', function () {
+    if (Auth::check()) {
+        return view('test');
+    }
+    else {
+        return view('welcome');
+    }
+});
+
+Route::get('/middle', function () {
+    // Only authenticated users may enter...
+})->middleware('auth');
+
+Route::get('/login', function() {
+    return view('login');
+})->name('login');
